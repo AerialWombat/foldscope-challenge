44e0455688671426f2a63015aa263320d1dfcc6b
AerialWombat <adrianzhan@gmail.com>
2021-07-26 14:08:47 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { colors: defaultColors } = require('tailwindcss/defaultTheme');
 module.exports = {
   purge: {
     enabled: false,
-    content: ['./src/**/*.jsx', './public/index.html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     options: {
       safelist: ['bg-blue', 'bg-dark-orange', 'bg-light-orange', 'text-white', 'bg-white'],
     },
